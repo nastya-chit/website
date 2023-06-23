434e3128baff5bd8dde233506ae312d0f1a7ef14
LoGz0 <132328784+LoGz0@users.noreply.github.com>
2023-06-23 12:53:10 +1000

    слайд шоу

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-  $(document).ready(function() {
-	var slideIndex = 0;
-	showSlides();
-	function showSlides() {
-		var i;
-        var slides = $(".slideshow-container .slide");
-        for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-        }
-		slideIndex++;
-        if (slideIndex > slides.length) {
-          slideIndex = 1;
-        }
-        slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 4000); // Измените значение 3000 на интервал, который вам нужен для показа каждого слайда
-      }
-    });
+var slideIndex = 0;
+showSlides();
 
-$(document).ready(function() {
-	$('.menu-button').hover(function() {
-		$('.dropdown-menu').stop().slideDown(200);
-	}, function() {
-		$('.dropdown-menu').stop().slideUp(200);
-	});
-});
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000);
+}
